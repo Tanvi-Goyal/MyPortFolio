50d5b4def15798494b5aa76bd0cc9c89a6126b2f
Tanvi Goyal <goyaltanvi94@gmail.com>
2018-12-19 22:34:15 +0530

    Added Graph Library

--- app/src/main/java/com/example/tanvi/myportfolio/AboutFragment.java
+++ app/src/main/java/com/example/tanvi/myportfolio/AboutFragment.java
@@ -7,6 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.animation.ChartAnimationListener;
+import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.Chart;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class AboutFragment extends Fragment {
 
@@ -19,12 +35,164 @@ public class AboutFragment extends Fragment {
         return fragment;
     }
 
+    private LineChartView chart;
+    private LineChartData data;
+    private int numberOfLines = 1;
+    private int maxNumberOfLines = 4;
+    private int numberOfPoints = 12;
+
+    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
+
+    private boolean hasAxes = true;
+    private boolean hasAxesNames = true;
+    private boolean hasLines = true;
+    private boolean hasPoints = true;
+    private ValueShape shape = ValueShape.CIRCLE;
+    private boolean isFilled = true;
+    private boolean hasLabels = true;
+    private boolean isCubic = false;
+    private boolean hasLabelForSelected = true;
+    private boolean pointsHaveDifferentColor = true;
+    private boolean hasGradientToTransparent = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        chart = (LineChartView) view.findViewById(R.id.chart);
+        chart.setOnValueTouchListener(new ValueTouchListener());
+
+        displayGraph();
+
+        return view ;
     }
 
+    private void displayGraph() {
+
+        generateValues();
+        generateData();
+        toggleCubic();
+
+        resetViewport();
+    }
+
+    private void generateValues() {
+        for (int i = 0; i < maxNumberOfLines; ++i) {
+            for (int j = 0; j < numberOfPoints; ++j) {
+                randomNumbersTab[i][j] = (float) Math.random() * 100f;
+            }
+        }
+    }
+
+    private void resetViewport() {
+        // Reset viewport height range to (0,100)
+        final Viewport v = new Viewport(chart.getMaximumViewport());
+        v.bottom = 0;
+        v.top = 100;
+        v.left = 0;
+        v.right = numberOfPoints - 1;
+        chart.setMaximumViewport(v);
+        chart.setCurrentViewport(v);
+    }
+
+    private void toggleCubic() {
+        isCubic = !isCubic;
+
+        generateData();
+
+        if (isCubic) {
+
+            final Viewport v = new Viewport(chart.getMaximumViewport());
+            v.bottom = -5;
+            v.top = 105;
+            chart.setMaximumViewport(v);
+            chart.setCurrentViewportWithAnimation(v);
+        } else {
+            final Viewport v = new Viewport(chart.getMaximumViewport());
+            v.bottom = 0;
+            v.top = 100;
+            chart.setViewportAnimationListener(new ChartAnimationListener() {
+
+                @Override
+                public void onAnimationStarted() {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void onAnimationFinished() {
+                    chart.setMaximumViewport(v);
+                    chart.setViewportAnimationListener(null);
+
+                }
+            });
+            chart.setCurrentViewportWithAnimation(v);
+        }
+
+    }
+
+    private void generateData() {
+
+        List<Line> lines = new ArrayList<Line>();
+        for (int i = 0; i < numberOfLines; ++i) {
+
+            List<PointValue> values = new ArrayList<PointValue>();
+            for (int j = 0; j < numberOfPoints; ++j) {
+                values.add(new PointValue(j, randomNumbersTab[i][j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(ChartUtils.COLORS[i+4]);
+            line.setShape(shape);
+            line.setCubic(isCubic);
+            line.setFilled(isFilled);
+            line.setHasLabels(hasLabels);
+            line.setHasLabelsOnlyForSelected(hasLabelForSelected);
+            line.setHasLines(hasLines);
+            line.setHasPoints(hasPoints);
+            if (pointsHaveDifferentColor){
+                line.setPointColor(ChartUtils.COLORS[(i+4) % ChartUtils.COLORS.length]);
+            }
+            lines.add(line);
+        }
+
+        data = new LineChartData(lines);
+
+        if (hasAxes) {
+            Axis axisX = new Axis();
+            Axis axisY = new Axis();
+            if (hasAxesNames) {
+                axisX.setName("Years").setTextSize(13).setTextColor(R.color.black);
+                axisY.setName("Performance").setTextSize(13).setTextColor(R.color.black);
+
+            }
+            data.setAxisXBottom(axisX);
+//            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+//        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        chart.setLineChartData(data);
+
+    }
+
+    private class ValueTouchListener implements LineChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int lineIndex, int pointIndex, PointValue value) {
+            Toast.makeText(getActivity(), "Selected: " + value, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onValueDeselected() {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
 
 }
